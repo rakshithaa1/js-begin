69bf310792a61a7f40d092884f015861a0aacc04
Rakshitha <125643699+rakshithaa1@users.noreply.github.com>
2026-01-07 23:38:56 +0000

    A clear and concise description of your changes

---
+++ 01_basics/var.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="raks@gmail.com"
+var accountPassword="12345"
+let accountState;
+
+// accountId=2
+accountEmail="rrrr@gmail.com"
+accountPassword="212121"
+accountCity="Bangalore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
