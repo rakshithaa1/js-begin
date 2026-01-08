1fb0472484920627b76a30e5f63eb3a29946b595
Rakshitha <125643699+rakshithaa1@users.noreply.github.com>
2026-01-08 00:35:11 +0000

    conversion

---
+++ 01_basics/conoper.js
@@ -0,0 +1,8 @@
+let score="33ab"
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
