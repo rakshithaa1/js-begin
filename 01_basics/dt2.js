3a1903a91db330213d528e5eb4724a665f91e6dc
Rakshitha <125643699+rakshithaa1@users.noreply.github.com>
2026-01-10 05:15:16 +0000

    dt summary

---
+++ 01_basics/dt2.js
@@ -0,0 +1,17 @@
+
+const id=Symbol("12")
+const id2=Symbol("12")
+// console.log(id===id2);
+// console.log(id==id2);
+const flowers=["rose","daisy","jasmine"]
+console.log(flowers);
+{
+    name:"raks";
+    age:23;
+}
+console.log();
+
+let myFunc=function(){
+    console.log("hello world");
+    
+}
